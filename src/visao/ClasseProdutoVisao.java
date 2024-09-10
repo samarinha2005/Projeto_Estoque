3555ecc35534877c2dcc74231e80a7aecc30c6e7
Samara <samara.oliveira@outlook.com>
2024-09-10 10:13:17 -0400

    Primeiro commit do projeto

---
+++ src/visao/ClasseProdutoVisao.java
@@ -0,0 +1,23 @@
+package visao;
+
+import java.util.ArrayList;
+
+import dominio.ClasseProduto;
+import repositorio.ClasseProdutoRepo;
+
+public class ClasseProdutoVisao {
+    public void Exibir() {
+        ClasseProdutoRepo repo = new ClasseProdutoRepo();
+        ArrayList<ClasseProduto> lista = repo.Browse();
+        for (ClasseProduto cp : lista) {
+            this.Imprimir(cp);
+        }
+    }
+
+    private void Imprimir(ClasseProduto alvo) {
+        System.out.println("Classe de Produto:");
+        System.out.println("Código: " + alvo.getCodigo());
+        System.out.println("Descrição: " + alvo.getDescricao());
+        System.out.println("Data de Inclusão: " + alvo.getDataDeInclusao());
+    }
+}
